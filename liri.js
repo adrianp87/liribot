35d30f5cf78a2f6661b84ed99ce1c53d00e272b5
Adrian Perez <adrian.perez87@gmail.com>
2019-07-24 09:14:12 -0700

    added pseudocode

---
+++ liri.js
@@ -0,0 +1,11 @@
+//get spotify keys
+
+//create function to search for songs and return info
+
+// create function to search bands in town and return info
+
+// create a for loop 
+
+// create function to search for OMBD and return info
+
+//create a function to split items read from the random.txt file
